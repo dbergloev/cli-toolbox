befe490877033de12574bb10504d472679e52790
Daniel Bergløv <d.bergloev@cloudhome.dk>
2026-01-17 10:47:14 +0100

    Add functions to make the code more managable

--- zfs/rc.cron.d/diskmon@hourly.sh
+++ zfs/rc.cron.d/diskmon@hourly.sh
@@ -23,101 +23,143 @@ esac
 ###
 #
 #
-declare -a devices
-declare state
-
-for pool in $(zpool list -H -o name); do
-    state=0
-    echo "Checking ZFS pool '$pool' status"
+chk_pool_health() {
+    local pool="$1"
+    local status="$(zpool list -H -o health "$pool")"
     
-    if [[ "$(zpool list -H -o health $pool)" != "ONLINE" ]]; then
-        echo "Critical : The zpool '$pool' needs your attention"
-        /usr/bin/alert --priority "critical" --title "ZFS Health Check" --id zfs_health_$pool --timeout 86400 "The zpool '$pool' needs your attention"
-        state=1
+    echo "Checking pool health on '$pool'"
+    
+    if [[ "$status" != "ONLINE" ]]; then
+        echo "    Warning: The health on '$pool' is '$status'" >&2
+        /usr/bin/alert --priority "critical" --title "ZFS Health Check" --id zfs_health_$pool --timeout 86400 "The health on '$pool' is '$status'"
         
     else
-        for device in $(zpool status -PL $pool | grep -oe '/dev/.*$' | awk '{print $1}'); do
-            echo "Checking device '$device' beloning to ZFS pool '$pool'"
-        
-            device=$(blksrc $device)                                # Convert something like /dev/dm-1 -> /dev/sdb1
-            device=/dev/$(lsblk -no pkname $device | tail -n 1)     # Convert something like /dev/sdb1 -> /dev/sdb
-            
-            if [[ -n "$device" && -b $device ]]; then
-                echo "    Realpath: $device"
-            
-                if [[ ! " ${devices[*]} " =~ " $(basename $device) " ]]; then
-                    if smartctl-overlay -i $device 2>/dev/null | grep -Eq 'SMART support is:[[:space:]]*Enabled'; then
-                        read -r dev_type dev_temp <<< "$(
-                          smartctl-overlay -A "$device" 2>/dev/null | awk '
-                            # ATA / SATA / USB
-                            $1 == 194 { print "ATA", $10; exit }
-                            $1 == 190 { print "ATA", $10; exit }
-
-                            # NVMe
-                            /^Temperature:[[:space:]]*[0-9]+/ {
-                                print "NVMe", $2
-                                exit
-                            }
-
-                            # SAS
-                            /Current Drive Temperature:/ {
-                                print "SAS", $4
-                                exit
-                            }
-                          '
-                        )"
-                        
-                        if { [[ "$dev_type" == "SAS"  ]] && (( dev_temp >= 55 )); } ||
-                           { [[ "$dev_type" == "ATA"  ]] && (( dev_temp >= 50 )); } ||
-                           { [[ "$dev_type" == "NVMe" ]] && (( dev_temp >= 70 )); }
-                        then
-                            echo "The $dev_type device $device is above threshold at $dev_temp°C"
-                            /usr/bin/alert --priority "critical" --title "SmartCTL Health Check" --id zfs_health_$(basename $device) --timeout 86400 "The $dev_type device $device is above temperature threshold at $dev_temp°C"
-                    
-                        else
-                            echo "The $dev_type device $device is below temperature threshold at $dev_temp°C"
-                            /usr/bin/alert --priority "low" --title "SmartCTL Health Check" --id zfs_health_$(basename $device) --timeout reset "The $dev_type device $device is below temperature threshold at $dev_temp°C"
-                        fi
-                        
-                        health="$(smartctl-overlay -H $device | grep overall-health)"
-                    
-                        if grep -q ' result:' <<< "$health" && ! grep -q 'PASSED' <<< "$health"; then
-                            devices+=( $(basename $device) )
-                            
-                            echo "Critical : SmartCTL Health Check" "The device '$device' that belongs to zpool '$pool' is in danger"
-                            /usr/bin/alert --priority "critical" --title "SmartCTL Health Check" --id zfs_health_$pool --timeout 86400 "The device '$device' that belongs to zpool '$pool' is in danger"
-                            state=1
-                        fi
-                    fi
-                fi
-            fi
-        done
+        echo "    The health on '$pool' is normal"
+        /usr/bin/alert --priority "low" --title "ZFS Health Check" --id zfs_health_$pool --timeout reset "The health on '$pool' has returned to normal"
     fi
+}
+
+###
+#
+#
+chk_pool_usage() {
+    local pool="$1"
+    local level=
     
-    if [[ $state -eq 0 ]]; then
-        /usr/bin/alert --id zfs_health_$pool --timeout reset
-    fi
-    
-    echo "Checking disk usage"
+    echo "Checking disk usage on '$pool'"
     read CAP FRAG <<< $(zpool list -H -o cap,frag $pool | tr -d '%')
 
-    if (( CAP >= 94 )); then
+    if (( CAP >= 90 )); then
       level="critical"
-    elif (( CAP >= 85 && FRAG >= 35 )); then
+    elif (( CAP >= 80 && FRAG >= 35 )); then
       level="high"
-    elif (( CAP >= 70 && FRAG >= 45 )); then
+    elif (( CAP >= 70 && FRAG >= 55 )); then
       level="medium"
-    elif (( CAP < 70 && FRAG >= 55 )); then
-      # Strange behaviour, give notice!
-      level="low"
-    else
-      level=""
     fi
     
     if [[ -n "$level" ]]; then
+        echo "    Warning: The zpool '$pool' exceeds ${CAP}% usage with ${FRAG}% fragmentation" >&2
         /usr/bin/alert --priority $level --title "ZFS Health Check" --id zfs_usage_$pool --timeout 86400 "The zpool '$pool' exceeds ${CAP}% usage with ${FRAG}% fragmentation"
         
     else
+        echo "    Usage on '$pool' is normal at ${CAP}% usage with ${FRAG}% fragmentation"
         /usr/bin/alert --id zfs_usage_$pool --timeout reset
     fi
+}
+
+###
+#
+#
+chk_device_health() {
+    local pool="$1"
+    local device="$2"
+    local health="$(smartctl-overlay -H $device | grep overall-health)"
+    
+    echo "Checking device health on '$device'"
+
+    if grep -q ' result:' <<< "$health" && ! grep -q 'PASSED' <<< "$health"; then
+        echo "    Warning: The disk '$device' from '$pool' did not pass SMART health check" >&2
+        /usr/bin/alert --priority "critical" --title "Disk Health Check" --id disk_health_$device --timeout 86400 "The disk '$device' from '$pool' did not pass SMART health check"
+        
+    else
+        echo "    The disk '$device' from '$pool' successfully passed SMART health check"
+        /usr/bin/alert --id disk_health_$device --timeout reset
+    fi
+}
+
+###
+#
+#
+chk_device_temp() {
+    local pool="$1"
+    local device="$2"
+    local dev_type dev_temp
+    
+    echo "Checking temperature on '$device'"
+
+    read -r dev_type dev_temp <<< "$(
+      smartctl-overlay -A "$device" 2>/dev/null | awk '
+        # ATA / SATA / USB
+        $1 == 194 { print "ATA", $10; exit }
+        $1 == 190 { print "ATA", $10; exit }
+
+        # NVMe
+        /^Temperature:[[:space:]]*[0-9]+/ {
+            print "NVMe", $2
+            exit
+        }
+
+        # SAS
+        /Current Drive Temperature:/ {
+            print "SAS", $4
+            exit
+        }
+      '
+    )"
+    
+    if { [[ "$dev_type" == "SAS"  ]] && (( dev_temp >= 55 )); } ||
+       { [[ "$dev_type" == "ATA"  ]] && (( dev_temp >= 50 )); } ||
+       { [[ "$dev_type" == "NVMe" ]] && (( dev_temp >= 70 )); }
+    then
+        echo "    Warning: The disk '$device' from '$pool' is above temperature threshold for $dev_type devices at $dev_temp°C" >&2
+        /usr/bin/alert --priority "critical" --title "Disk Health Check" --id disk_temp_$device --timeout 86400 "The disk '$device' from '$pool' is above temperature threshold for $dev_type devices at $dev_temp°C"
+
+    else
+        echo "    The disk '$device' from '$pool' is below temperature threshold at $dev_temp°C"
+        /usr/bin/alert --priority "low" --title "Disk Health Check" --id disk_temp_$device --timeout reset "The disk '$device' from '$pool' is below temperature threshold at $dev_temp°C"
+    fi
+}
+
+###
+#
+#
+declare -a devices
+
+for pool in $(zpool list -H -o name); do
+    echo "Running disk monitoring on the ZFS pool '$pool'"
+
+    chk_pool_health $pool
+    chk_pool_usage $pool
+    
+    for device in $(zpool status -PL $pool | grep -oe '/dev/.*$' | awk '{print $1}'); do
+        device=$(blksrc $device)                                # Convert something like /dev/dm-1 -> /dev/sdb1
+        device=/dev/$(lsblk -no pkname $device | tail -n 1)     # Convert something like /dev/sdb1 -> /dev/sdb
+        
+        if [[ -n "$device" && -b $device ]]; then
+            if [[ ! " ${devices[*]} " =~ " $(basename $device) " ]]; then
+                devices+=( $(basename $device) )
+                
+                echo "Evaluating SMART status of the device $device"
+                
+                if smartctl-overlay -i $device 2>/dev/null | grep -Eq 'SMART support is:[[:space:]]*Enabled'; then
+                    chk_device_health $pool $device
+                    chk_device_temp $pool $device
+                
+                else
+                    echo "    SMART is not enabled on the device $device"
+                fi
+            fi
+        fi
+    done
 done
+
